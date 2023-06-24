4df319e4a672dfdc1a0bb6aea7ea4b0a110d0321
nathanieladiah <nathanieladiah@gmail.com>
2023-06-24 08:05:02 -0400

    Update a note

--- index.js
+++ index.js
@@ -94,6 +94,21 @@ app.post("/api/notes", (request, response) => {
   });
 });
 
+app.put("/api/notes/:id", (request, response, next) => {
+  const body = request.body;
+
+  const note = {
+    content: body.content,
+    important: body.important,
+  };
+
+  Note.findByIdAndUpdate(request.params.id, note, { new: true })
+    .then((updatedNote) => {
+      response.json(updatedNote);
+    })
+    .catch((error) => next(error));
+});
+
 app.use(unknownEndpoint);
 
 app.use(errorHandler);

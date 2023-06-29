55fbd56d60a750ba5a72247eda90db056421e450
nathanieladiah <nathanieladiah@gmail.com>
2023-06-29 05:12:09 -0400

    Refactor project structure to adhere to best practices

---
+++ app.js
@@ -0,0 +1,33 @@
+const cors = require("cors");
+const express = require("express");
+const mongoose = require("mongoose");
+
+const notesRouter = require("./controllers/notes");
+const config = require("./utils/config");
+const middleware = require("./utils/middleware");
+const logger = require("./utils/logger");
+
+const app = express();
+
+mongoose.set("strictQuery", false);
+
+logger.info("connecting to", config.MONGODB_URI);
+
+mongoose
+  .connect(config.MONGODB_URI)
+  .then(() => {
+    logger.info("connected to MongoDb");
+  })
+  .catch((error) => {
+    logger.error("error connecting to MongoDb:", error.message);
+  });
+
+app.use(cors());
+app.use(express.static("build"));
+app.use(express.json());
+app.use(middleware.requestLogger);
+app.use("/api/notes", notesRouter);
+app.use(middleware.unknownEndpoint);
+app.use(middleware.errorHandler);
+
+module.exports = app;
